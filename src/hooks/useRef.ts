77bdea6b6c7057b8d5b222cb4735afee03e062e2
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-25 18:10:52 +0800

    Create useRef.ts

---
+++ src/hooks/useRef.ts
@@ -0,0 +1,79 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import useGuard, { Slot } from './useGuard';
+import { Optional } from '../utils/types';
+import { PayloadMismatchError } from '../utils/exceptions';
+
+type MUTABLE_REF = 'MUTABLE_REF';
+
+export interface MutableRef<T> {
+  current: T,
+}
+
+interface MutableRefSlot<T> extends Slot<MUTABLE_REF, MutableRef<T>>{
+}
+
+const MEMORY_SIZE = 1;
+
+export default function useRef<T>(initialValue: T): MutableRef<T> {
+  return useGuard<MutableRef<T>>((reader) => {
+    // read at the current position
+    const slot: Optional<Slot<any, any>> = reader.read(MEMORY_SIZE)[0];
+
+    // check if slot has a value
+    if (slot) {
+      // check if payload matches
+      if (slot.type !== 'MUTABLE_REF') {
+        throw new PayloadMismatchError(slot.type, 'MUTABLE_REF');
+      }
+
+      // move cursor
+      reader.move(MEMORY_SIZE);
+
+      // return value
+      return (slot as MutableRefSlot<T>).value;
+    }
+
+    // initialize slot
+    const ref: MutableRefSlot<T> = {
+      type: 'MUTABLE_REF',
+      value: {
+        current: initialValue,
+      },
+    };
+
+    // write to slot
+    reader.write(slot);
+
+    // move cursor
+    reader.move(MEMORY_SIZE);
+
+    // return reference
+    return ref.value;
+  })
+}
